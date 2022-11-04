61ba491943a8bd9a52d26cca422a7cb0cf3988c7
Ron Olson <tachoknight@gmail.com>
2022-11-03 21:12:21 -0500

    Added headless option so the terminal window doesn't retreat to the side with Stage Manager

--- check_xcode_version.py
+++ check_xcode_version.py
@@ -2,8 +2,13 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
-firefox = webdriver.Firefox()
+
+options = Options()
+options.add_argument("--headless")
+
+firefox = webdriver.Firefox(options=options)
 firefox.get('https://en.wikipedia.org/wiki/Xcode')
 
 b = firefox.find_elements(By.CLASS_NAME, 'infobox-data')
